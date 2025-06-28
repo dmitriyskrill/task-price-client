0e801cf2a355a8bbe29fb6c74e57bdbc5ccc86de
Sashmorra <lebedev2008.a@gmail.com>
2025-06-28 13:44:08 +0300

    dev

--- src/modules/taskDateGraph/api/postTaskDateGraph.ts
+++ src/modules/taskDateGraph/api/postTaskDateGraph.ts
@@ -3,7 +3,7 @@ import type { ITaskDateGraph } from '../types/TaskDateGraph.interface'
 
 export async function postTaskDateGraph (data: Partial<ITaskDateGraph>) {
   try {
-    const res = await axiosWithAuth.post('/taskDateGraph', data)
+    const res = await axiosWithAuth.post('/task-date-graph', data)
     return res.data
   } catch (e) {
     return null
